065a1bfd6a62da977d4e4936fed75df7d3e6f38b
Jimmy Åkesson <bazzak81@gmail.com>
2019-06-18 20:49:35 +0200

    Asteroid and Destroy

---
+++ Space_Shooter/Assets/Scripts Code/DestroyByContact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explotion;
+    public GameObject playerExplotion;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        Instantiate(explotion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplotion, transform.position, transform.rotation);
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
